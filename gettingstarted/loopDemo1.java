44a39ba40ec1143a97d036cc5bbddd721691d0de
paragniist <paragniist@gmail.com>
2021-08-15 15:06:56 +0530

    demo : while and for loop

--- gettingstarted/loopDemo1.java
+++ gettingstarted/loopDemo1.java
@@ -1,19 +1,17 @@
-package gettingstarted;
-
-java.util.*;
 public class loopDemo1{
 
-    public static void main(String[] args) {
-        int i =1;
-        while(i <= 9){
-            System.out.println(i);
-            i = i + 1;
+    public static void main(String []args){
+       /*int i =1;
+       while(i <= 9){
+           System.out.println(i);
+           i = i + 1;
 
-        }
-        System.out.println("Done.!!");
+       }*/
+       for(int i=1; i<=128; i=i*3)
+       System.out.println(i);
+    
+        System.out.println("Done!!");
     }
-
 }
 
 
-//
